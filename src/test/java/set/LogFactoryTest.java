c515edaa95db682e5a872d1c29c7837698dbf6eb
RoyYoung <976611019@qq.com>
2019-04-28 19:15:33 +0800

    init

---
+++ src/test/java/set/LogFactoryTest.java
@@ -0,0 +1,15 @@
+package set;
+
+import org.junit.Test;
+
+public class LogFactoryTest {
+    @Test
+    public void getFactory() {
+        Thread.currentThread().setContextClassLoader(null);
+    }
+
+    @Test
+    public void getContextClassLoaderInternal() {
+
+    }
+}
